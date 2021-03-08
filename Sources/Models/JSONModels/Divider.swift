75d6cfa9e95cabad97923b1106732a2693d145d4
Ben Shutt <ben@3sidedcube.com>
2021-03-08 16:54:19 +0000

    Update `Image` and `Divider`

--- Sources/Models/JSONModels/Divider.swift
+++ Sources/Models/JSONModels/Divider.swift
@@ -7,17 +7,14 @@
 
 import Foundation
 
-/// A structure with merely a background color and a fixed height
+/// A structure used as a spacer or divider.
 public struct Divider: Codable, JSONModel {
 
     /// `RGBAHex` color of the divider
-    public var dividerColor: RGBAHex?
+    public var color: RGBAHex?
 
     /// `Float` height of the divider
-    public var dividerHeight: Float?
-
-    /// `Padding` for divider insets
-    public var padding: Padding?
+    public var strokeWidth: Float?
 
     /// `Margin` for insets relative to other content
     public var margin: Margin?
--- Sources/Models/JSONModels/Image.swift
+++ Sources/Models/JSONModels/Image.swift
@@ -14,15 +14,28 @@ public struct Image: Codable, JSONModel {
     /// `String` remote URL location
     public var src: ImageSource?
 
-    /// `RGBAHex` color to use for a placeholder while the remote image is being fetched
-    public var placeholderColor: RGBAHex?
+    // MARK: - View
+
+    /// RGBA hex color for the background
+    public var backgroundColor: RGBAHex?
 
     /// `Padding` for text insets
     public var padding: Padding?
 
     /// `Margin` for insets relative to other content
     public var margin: Margin?
+
+    /// `Shadow` of container view
+    public var shadow: Shadow?
+
+    /// Corner radius of container view in points
+    public var cornerRadius: Float?
+
+    /// `Border` of the container
+    public var border: Border?
 }
+
+// MARK: - Extensions
 
 public extension Image {
 
